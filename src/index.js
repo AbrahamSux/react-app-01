3db2f9fa74e8c11f44ebc5e772757a2fc6a1b01f
AbrahamJuarez <ajuarezdelacruz93@gmail.com>
2022-04-15 18:44:00 -0500

    Nombre del Proyecto: Sección 4: React Core ( Guia )
    Observaciones y/o Comentarios: 20. InIcializadores de propiedad (property Initializers). Se agrega soporte.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import Example02 from './components/Example02'
 import Example03 from './components/Example03'
 import Example04 from './components/Example04'
 import Example05 from './components/Example05'
+import Example06 from './components/Example06'
 
 /* const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,4 +17,4 @@ root.render(
   </React.StrictMode>
 ); */
 
-ReactDOM.render(<Example05/>, document.getElementById('root'))
+ReactDOM.render(<Example06/>, document.getElementById('root'))
