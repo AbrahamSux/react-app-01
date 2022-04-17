24194c75fad73b0d548840ac6686e31841c84a42
AbrahamJuarez <ajuarezdelacruz93@gmail.com>
2022-04-17 17:36:32 -0500

    Nombre del Proyecto: Sección 4: React Core ( Guia )
    Observaciones y/o Comentarios: 42. Entendiendo los portals de React.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+// COMPONENTES CON ESTADO
+
+class PortalModalApp extends Component {
+
+  // RENDER
+
+  /**
+   * Utilizado para renderizar los elementos.
+   * 
+   * @returns - El elemento construido.
+   */
+  render() {
+    console.log(this.props)
+
+    // Si NO recibe la propiedad 'visible' no se mostrará nada del modal en pantalla.
+    if (!this.props.visible) {
+      return null
+    }
+
+    // En caso contrario, mostrará lo siguiente:
+
+    const styles = {
+      width: '100%',
+      height: '100%',
+      position: 'absolute',
+      top: '0',
+      left: '0',
+      background: 'linear-gradient(to top right, #667eea, #764ba2',
+      opacity: '0.9',
+      color: '#FFF'
+    }
+
+    return ReactDOM.createPortal(
+      // Marcado/Elemento a Montar.
+      (
+        <div style={styles}>
+          <h3>Portal Modal</h3>
+          <hr/>
+          {this.props.children}
+        </div>
+      ),
+      // Puntero hacia donde se va a montar.
+      document.getElementById('modal-root')
+    );
+  }
+
 }
+
+class App extends Component {
+
+  state = {
+    visible: false,
+    num: 0
+  }
+
+
+
+  // ARROW FUNCTIONS
+
+  mostrar = () => {
+    this.setState({
+      visible: true
+    })
+  }
+
+  cerrar = () => {
+    this.setState({
+      visible: false
+    })
+  }
+
+  componentDidMount = () => {
+    setInterval(() => {
+      this.setState(state => ({
+        num: state.num +1
+      })
+      )
+    }, 5000)
+  }
+
+
+
+  // RENDER
+
+  /**
+   * Utilizado para renderizar los elementos.
+   * 
+   * @returns - El elemento construido.
+   */
+  render() {
+    return (
+      <div>
+        <button onClick={this.mostrar}>
+          Mostrar
+        </button>
+        <PortalModalApp visible={this.state.visible}>
+          <h1>Hola desde un Portal Modal {this.state.num}</h1>
+          <br/>
+          <button onClick={this.cerrar}>
+            Cerrar
+          </button>
+        </PortalModalApp>
+      </div>
+    )
+  }
+
+}
+
+
+
+// EXPORT
 
 export default App;
